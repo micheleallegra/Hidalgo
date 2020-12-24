6edad3c0c3a5ff6553d093f34071a38a5db5f237
Giovanni <9051577+giovannidoni@users.noreply.github.com>
2020-12-24 20:19:14 +0000

    Install instead of simple build

--- python/install.sh
+++ python/install.sh
@@ -1,5 +1,4 @@
 nppath=`python -c 'import numpy; print(numpy.get_include())'`
 export PYTHONPATH=$PYTHONPATH:./lib/python/
 CFLAGS="-I $nppath/numpy" python setup.py install --home=./
-python setup.py build
-cp build/lib.macosx-10.9-x86_64-3.8/_gibbs.cpython-38-darwin.so ./dimension/_gibbs.so
+python setup.py install
