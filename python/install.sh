0b66663295067f1c908cbf61738e4dab39c8c951
micheleallegra <micheleallegra85@gmail.com>
2022-05-17 20:20:34 +0200

    Update install.sh

--- python/install.sh
+++ python/install.sh
@@ -3,7 +3,7 @@ nppath=$nppath/core/include/numpy
 mkdir lib
 mkdir lib/python
 export PYTHONPATH=$PYTHONPATH:./lib/python/
-CFLAGS="-I $nppath" python setup.py install --home=./ 
+CFLAGS="-I $nppath" python3 setup.py install --home=./ 
 python3 setup.py build
 gs=`find build/ -name _gibbs.so`
 cp $gs dimension/
